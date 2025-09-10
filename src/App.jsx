bc156cafe9fa09866f7aedc074fc9815a0f1f758
nolanjace034-tech <nolanjace034@gmail.com>
2025-09-10 04:36:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,23 @@ export default function App() {
   return (
     <div style={{ fontFamily: 'sans-serif', padding: '20px' }}>
       <h1>Welcome to SimBank v2</h1>
-      <p>This is the Vercel-ready version of your simulated banking app.</p>
+      <p> banking app.</p>
+    </div>
+  )
+}
+import React from 'react'
+import TopBar from './TopBar'
+import Send from './Send'
+import Transactions from './Transactions'
+import Toast from './Toast'
+
+export default function App() {
+  return (
+    <div>
+      <TopBar />
+      <Send />
+      <Transactions />
+      <Toast />
     </div>
   )
 }
