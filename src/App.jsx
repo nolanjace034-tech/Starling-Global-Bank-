360dd24b76ce3cbb5bc87e940cad618ec08d5a03
nolanjace034-tech <nolanjace034@gmail.com>
2025-09-10 05:04:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,4 @@
 import React from 'react'
-
-export default function App() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', padding: '20px' }}>
-      <h1>Welcome to SimBank v2</h1>
-      <p> banking app.</p>
-    </div>
-  )
-}
-import React from 'react'
 import TopBar from './TopBar'
 import Send from './Send'
 import Transactions from './Transactions'
