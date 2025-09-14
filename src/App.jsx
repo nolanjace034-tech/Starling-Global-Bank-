cdb918ec272f7983bd3462efc22cdb2b71d7888b
nolanjace034-tech <nolanjace034@gmail.com>
2025-09-14 11:57:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
-export default function App() {
-  return <h1 style={{color:"red"}}>Hello from SimBank 🚀</h1>
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './Pages/Login.jsx'
+import Signup from './Pages/Signup.jsx'
+import Dashboard from './Pages/Dashboard.jsx'
+import AdminLogin from './Pages/AdminLogin.jsx'
+import AdminPanel from './Pages/AdminPanel.jsx'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin-panel" element={<AdminPanel />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
+
+export default App
